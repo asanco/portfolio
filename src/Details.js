08b9f8859738571cf28a27e52547644c52e455bb
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2019-01-30 16:52:54 -0500

    conection with drive done

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import profilePic from './image/PersonalFotoBW.jpg';
 
 class Details extends Component {
   render() {
     return (
       <div className="details fade-in">
-      <img src={profilePic} alt="Foto of me" style={{width:"95%"}}></img>
+      <img src={"https://docs.google.com/uc?id=1dr3nzco33xB5CdfqnjBwmHKit5ub3qql"} alt="Foto of me" style={{width:"95%"}}></img>
       <h5>Andrés Sandoval</h5>
       <h6>Computer Science and Engineering</h6>
       <p>Welcome to my portfolio. Check out my projects. You can see more details by clicking each of the cards.</p>
--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,6 @@ import './App.css';
 import Highlight from "./Highlight.js";
 import HighlightDetails from "./HighlightDetails.js";
 import Details from "./Details.js";
-import pic11 from './image/dice1.jpg';
-import pic21 from './image/MinesweeperTeamFoto2.PNG';
-import pic31 from './image/TreasureHunterFoto2.PNG';
-import pic41 from './image/RobotHRIFoto2.png';
-import pic61 from './image/PersonalWebsiteFoto2.png';
-
 class Main extends Component {
 
 constructor(props) {
@@ -17,27 +11,38 @@ constructor(props) {
   this.state = {
    highlightIndex: -1,
    url: '',
-   imgUrls : [
-  [ pic11, "Roll'em", "Dice game for mobile devices made using Unity", "Roll'em is a singleplayer dice game made with Unity. Rotate the die or dice to reach the target score before the time runs out. Still in alpha stage.", "Games", "https://google.com"],
-  [ pic21, "Minesweeper Team", "A multiplayer version of Minesweeper mande with Meteor", "Minesweeper Team is a web based multiplayer Minesweeper game made with the Meteor framework. Scores and finished games are stored in a database as well as a leaderboard with the best scores.", "Web Applications/Games", "https://github.com/jdguzmans/minesweeper-team", ""],
-  [ pic31, "Treasure Hunter", "A scavenger hunt application that uses Google Map's API", "Treasure Hunter is a web based treasure hunter game made with React. It makes use of Google Maps' API to find specific locations that are hinted at while looking for the treasure. You can also make your own Hunt in the creation menu.", "Web Applications/Games", "https://github.com/asandovaluniandes/treasure-hunter",""],
-  [ pic41, "Human Robot Interaction", "Application that lets a user control a robot with a Kinect", "This university project showcases the Kinect's ability to recognize customized gestures to move a P3-DX robot in a simulation environment and in real space.", "Robotics", "https://github.com",""],
-  [ pic41, "Robot Path Planning", "Path planning application that uses an A* algorithm", "Robotics project that implements two things: path planning using an A* algorithm to optimize it´s route to it's goal and localization using the robot's laser to correct it's internal position.", "Robotics", "https://github.com"],
-  [ pic61, 'Personal website', 'Basic HTML page',"My first web development project. It is a personal website that I made while learning HTML and CSS. It is made only with those two tecnologies.", "Web Dev", "https://github.com/asandovaluniandes/asandovaluniandes.github.io",""]
-   ]
+   imgUrls : []
   }
-  this.loadData("http://spreadsheets.google.com/feeds/list/12xVYa20sk-g9SETTDj2RixDDy5R7uEsUSONBYSoN6VY/od6/public/values?alt=json");
+  this.loadData("https://spreadsheets.google.com/feeds/list/12xVYa20sk-g9SETTDj2RixDDy5R7uEsUSONBYSoN6VY/od6/public/values?alt=json");
 }
 
  loadData(url) {
   var xhttp = new XMLHttpRequest();
+  var projects = [];
   var portfolioJson = null;
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState === 4 && xhttp.status === 200) {
       portfolioJson = JSON.parse(xhttp.responseText);
-      console.log(portfolioJson.feed.entry);
+      var jsonProjects = portfolioJson.feed.entry;
+      //console.log(jsonProjects);
+
+      for (var i=0; i<jsonProjects.length; i++){
+        var projectRow = [];
+        projectRow.push(jsonProjects[i].gsx$firstimg.$t);
+        projectRow.push(jsonProjects[i].gsx$title.$t);
+        projectRow.push(jsonProjects[i].gsx$shortdesc.$t);
+        projectRow.push(jsonProjects[i].gsx$longdesc.$t);
+        projectRow.push(jsonProjects[i].gsx$category.$t);
+        projectRow.push(jsonProjects[i].gsx$link.$t);
+        projects.push(projectRow);
+      }
+
+      //console.log(projects);
+      this.setState({
+       imgUrls: projects
+      })
     }
-  };
+  }.bind(this);
   xhttp.open("GET", url, true);
   xhttp.send();
 }
