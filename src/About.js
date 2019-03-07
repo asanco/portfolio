996ff873785ee67f82cbd06499736246f352dbbf
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2019-03-07 00:38:02 -0500

    content changes to about and contact

--- src/About.js
+++ src/About.js
@@ -4,10 +4,19 @@ import './App.css';
 class About extends Component {
   render() {
     return (
-      <div className="about fade-in">
-      <h4> I'm a last semester Computer Science and Engineering student at Universidad de los Andes in Bogotá, Colombia.</h4>
-      <h4> My main interest is in visual computing and virtual environments. I'm currently working on my final undergraduate project which integrates virtual reality and animation</h4>
-      <h4> For more information, check out my CV here</h4>
+      <div className="about fade-in row">
+      <div className = "col-md-6">
+        <h5> I'm a last semester Systems and Computing Engineering student at Universidad de los Andes in Bogotá, Colombia.</h5>
+        <h5> Among my interests are visual computing, video game development, front-end web development and robotics.</h5>
+        <h5> I'm currently working on my final undergraduate project which consists of creating real time animations with virtual reality equimenent and software.</h5>
+        <h5> I enjoy learing new languages (currently learning German) as well as editing videos and learning 3D animation.</h5>
+        <br/>
+        <br/>
+        <h5> If you want to check out my full CV, click here.</h5>
+      </div>
+      <div className = "col-md-6">
+        <img src={"https://docs.google.com/uc?id=1BlJNiJwihj5vsjoQ4SQoDOkPrvEdWdtt"} alt="Foto of me" style={{width:"100%"}}></img>
+      </div>
       </div>
     );
   }
--- src/Contact.js
+++ src/Contact.js
@@ -5,10 +5,22 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact fade-in">
-      <h4> <i className="fab fa-linkedin-in"> </i><a href = "http://linkedin.com/in/asandoval110"> http://linkedin.com/in/asandoval110 </a></h4>
-      <h4> <i className="fab fa-github"></i><a href = "https://github.com/asandovaluniandes"> https://github.com/asandovaluniandes </a></h4>
-      <h4> <i class="fas fa-envelope"></i><a href = "mailto: a.sandoval1303@uniandes.edu.co"> a.sandoval1303@uniandes.edu.co </a></h4>
-
+      <h4> You can contact me via these links:</h4>
+      <br/>
+      <div className="row" style={{textAlign: "center"}}>
+        <div className = "col-md-4">
+          <h3><a href = "http://linkedin.com/in/asandoval110"><i className="fab fa-linkedin-in"> </i></a></h3>
+          <h5><a href = "http://linkedin.com/in/asandoval110">asandoval110 </a></h5>
+        </div>
+        <div className = "col-md-4">
+          <h3><a href = "https://github.com/asandovaluniandes"><i className="fab fa-github"></i></a></h3>
+          <h5><a href = "https://github.com/asandovaluniandes">asandovaluniandes </a></h5>
+        </div>
+        <div className = "col-md-4">
+          <h3><a href = "mailto: a.sandoval1303@uniandes.edu.co"><i class="fas fa-envelope"></i></a></h3>
+          <h5><a href = "mailto: a.sandoval1303@uniandes.edu.co">@uniandes.edu.co </a></h5>
+        </div>
+      </div>
       </div>
     );
   }
