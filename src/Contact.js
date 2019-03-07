5d1a7ca771094a8992d4c2a6938be7398213097a
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2019-03-06 23:54:58 -0500

    changes to contact

--- src/Contact.js
+++ src/Contact.js
@@ -5,9 +5,9 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact fade-in">
-      <h4> <i className="fab fa-linkedin-in"> </i> LinkedIn: http://linkedin.com/in/asandoval110</h4>
-      <h4> <i className="fab fa-github"></i> GitHub: https://github.com/asandovaluniandes</h4>
-      <h4>E-mail: a.sandoval1303@uniandes.edu.co</h4>
+      <h4> <i className="fab fa-linkedin-in"> </i><a href = "http://linkedin.com/in/asandoval110"> http://linkedin.com/in/asandoval110 </a></h4>
+      <h4> <i className="fab fa-github"></i><a href = "https://github.com/asandovaluniandes"> https://github.com/asandovaluniandes </a></h4>
+      <h4> <i class="fas fa-envelope"></i><a href = "mailto: a.sandoval1303@uniandes.edu.co"> a.sandoval1303@uniandes.edu.co </a></h4>
 
       </div>
     );
