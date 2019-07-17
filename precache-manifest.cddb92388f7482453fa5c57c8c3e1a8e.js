42faa044b57dde621966f1b68f9a0a77fc8d0a34
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2019-07-16 22:39:31 -0500

    Updates

---
+++ precache-manifest.cddb92388f7482453fa5c57c8c3e1a8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "3a60cee0a71f7834ba35",
+    "url": "/portfolio/static/js/main.3a60cee0.chunk.js"
+  },
+  {
+    "revision": "1a322d7aad6bdbb45d24",
+    "url": "/portfolio/static/js/1.1a322d7a.chunk.js"
+  },
+  {
+    "revision": "3a60cee0a71f7834ba35",
+    "url": "/portfolio/static/css/main.bf434d10.chunk.css"
+  },
+  {
+    "revision": "92aec335070955a7787f25b0029f8a1f",
+    "url": "/portfolio/index.html"
+  }
+];
